bdda4625b0bd175c5f3bf7c22dbdf9b0b9aea4ef
GerrySoft <gerard@ricebes.cat>
2020-08-05 15:05:08 +0200

    aplicació per veure webcam

---
+++ webcam/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace webcam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(479, 307);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 307);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "webcam";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ webcam/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace webcam
+{
+    public partial class Form1 : Form
+    {
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        { 
+            var d = cam.VideoCapabilities;
+
+            if (keyData == Keys.Up)
+            {
+                this.Width += 5;
+                this.Height = (int)(this.Width * (float)d[5].FrameSize.Height / (float)d[5].FrameSize.Width);
+                return true;
+            }
+            if (keyData == Keys.Down)
+            {
+                this.Width -= 5;
+                this.Height = (int)(this.Width * (float)d[5].FrameSize.Height / (float)d[5].FrameSize.Width);
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+
+
+        VideoCaptureDevice cam;
+
+        public Form1()
+        {
+            InitializeComponent();
+            var dev = new FilterInfoCollection(FilterCategory.VideoInputDevice)[0];
+
+            cam = new VideoCaptureDevice(dev.MonikerString);
+            var d = cam.VideoCapabilities;
+
+            cam.VideoResolution = d[5];
+
+            this.Height = (int)(this.Width * (float)d[5].FrameSize.Height / (float)d[5].FrameSize.Width);
+
+            cam.NewFrame += Cam_NewFrame;
+            cam.Start();
+        }
+
+        private void Cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (cam.IsRunning) cam.Stop();
+        }
+
+        // per moure-ho
+        bool down = false;
+        int x, y;
+        private void MouseDown(object sender, MouseEventArgs e)
+        {
+            if (!down)
+            {
+                down = true;
+                x = e.X;
+                y = e.Y;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (down)
+            {
+                int x1 = e.X;
+                int y1 = e.Y;
+
+                this.Left += x1 - x;
+                this.Top += y1 - y;
+                //x = x1;
+                //y = y1;
+            }
+        }
+
+        private void MouseUp(object sender, MouseEventArgs e)
+        {
+            down = false;
+        }
+    }
+}
